bb9c36ce0296b39d1bc4506349d270e5f52ad700
cloud-zhao <wlsdhzy@163.com>
2015-07-17 17:08:22 +0800

    add partition

---
+++ partition_format.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+. /etc/profile
+
+disk=$1
+mount=$2
+
+fdisk $disk <<EOF
+n
+p
+1
+
+
+w
+EOF
+mkfs.ext4 ${disk}1
+mkdir -p $mount
+mount $disk $mount
+echo "$disk            $mount              ext4       defaults		 0 0">>/etc/fstab
+
+rm -rf /etc/cron.d/pat
