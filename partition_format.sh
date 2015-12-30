dbae99376157365d5c7302d9bce392be4995a614
cloud-zhao <wlsdhzy@163.com>
2015-12-30 12:54:43 +0800

    modify output to /dev/null

--- partition_format.sh
+++ partition_format.sh
@@ -12,7 +12,7 @@ p
 
 w
 EOF
-mkfs.ext4 ${disk}1
+mkfs.ext4 ${disk}1 >/dev/null 2>&1
 mkdir -p $mount
 mount $disk $mount
 echo "$disk            $mount              ext4       defaults		 0 0">>/etc/fstab
